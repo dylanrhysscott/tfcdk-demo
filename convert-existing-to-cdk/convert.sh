320f98d5e6b2ae2ed3860bd4ad4d4f4bb4a985c2
Dylan Scott <dylanrhysscott@gmail.com>
2023-06-18 16:31:44 +0100

    Show native CDK use and existing module

--- convert-existing-to-cdk/convert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Currently broken https://github.com/hashicorp/terraform-cdk/issues/2882
-cat main.tf | cdktf convert --provider hashicorp/aws --language go
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	existingvpc "cdk.tf/go/stack/generated/vpc"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/cdktf/cdktf-provider-aws-go/aws/v15/provider"
+	nativevpc "github.com/cdktf/cdktf-provider-aws-go/aws/v15/vpc"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+)
+
+type MultiStackConfig struct {
+	Region    string
+	useNative bool
+}
+
+func NewMultiStack(scope constructs.Construct, id string, name string, config MultiStackConfig) cdktf.TerraformStack {
+	var stack cdktf.TerraformStack
+	if config.useNative {
+		stack = cdktf.NewTerraformStack(scope, &id)
+		provider.NewAwsProvider(stack, jsii.String("AWS"), &provider.AwsProviderConfig{
+			Region: jsii.String(config.Region),
+		})
+		nativevpc.NewVpc(stack, jsii.String(name), &nativevpc.VpcConfig{
+			CidrBlock: jsii.String("10.100.0.0/16"),
+		})
+	} else {
+		stack = cdktf.NewTerraformStack(scope, &id)
+		provider.NewAwsProvider(stack, jsii.String("AWS"), &provider.AwsProviderConfig{
+			Region: jsii.String(config.Region),
+		})
+		existingvpc.NewVpc(stack, jsii.String(name), &existingvpc.VpcConfig{
+			Name: jsii.String(name),
+		})
+	}
+
+	return stack
+}
+
+func main() {
+	app := cdktf.NewApp(nil)
+	NewMultiStack(app, "native-module", "native-vpc", MultiStackConfig{
+		Region:    "eu-west-2",
+		useNative: true,
+	})
+	NewMultiStack(app, "existing-module", "existing-vpc", MultiStackConfig{
+		Region:    "eu-west-2",
+		useNative: false,
+	})
+	app.Synth()
+}
